10f8f8653b1515643dbafd9f60f77afa68ae0b3f
veehou <veehou@tencent.com>
2022-01-05 14:22:58 +0800

    [feat]sdk异步化，完成websocket的部分

--- examples/run_websocket_async.py
+++ examples/run_websocket_async.py
@@ -8,6 +8,22 @@ from qqbot.core.util.yaml_util import YamlUtil
 test_config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yaml"))
 
 
+async def _direct_message_handler(event, message: qqbot.Message):
+    """
+    定义事件回调的处理
+
+    :param event: 事件类型
+    :param message: 事件对象（如监听消息是Message对象）
+    """
+    msg_api = qqbot.MessageAPI(t_token, False)
+    # 打印返回信息
+    qqbot.logger.info("event %s" % event + ",receive message %s" % message.content)
+    # 构造消息发送请求数据对象
+    send = qqbot.MessageSendRequest("<@%s>谢谢你，加油" % message.author.id, message.id)
+    # 通过api发送回复消息
+    msg_api.post_direct_message(message.guild_id, send)
+
+
 async def _message_handler(event, message: qqbot.Message):
     """
     定义事件回调的处理
@@ -30,4 +46,7 @@ if __name__ == "__main__":
     qqbot_handler = qqbot.Handler(
         qqbot.HandlerType.AT_MESSAGE_EVENT_HANDLER, _message_handler
     )
+    # qqbot_dms_handler = qqbot.Handler(
+    #     qqbot.HandlerType.DIRECT_MESSAGE_EVENT_HANDLER, _direct_message_handler
+    # )
     qqbot.async_listen_events(t_token, False, qqbot_handler)
