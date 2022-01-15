0418f7938f6ac11754514626ee4ac383524e3e52
veehou <veehou@tencent.com>
2022-01-15 15:04:55 +0800

    fix: 修改频道列表api的请求对象参数问题

--- qqbot/model/user.py
+++ qqbot/model/user.py
@@ -2,12 +2,12 @@
 
 
 class ReqOption:
-    def __init__(self, before: str, after: str, limit: str):
+    def __init__(self, before: str = None, after: str = None, limit: str = None):
         """
         获取频道列表的请求
 
-        :param before: 读取此 id 之前的数据
-        :param after: 读取此 id 之后的数据
+        :param before: 读取此 guild id 之前的数据
+        :param after: 读取此 guild id 之后的数据
         :param limit: 每次拉取多少条数据，最大不超过 100，默认 100
         """
 
