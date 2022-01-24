dd107bec197f97382544dfcdcf220f28a7b95f48
veehou <veehou@tencent.com>
2022-01-24 10:59:12 +0800

    fix: 增加监听事件的数据对象在qqbot引入

--- qqbot/model/__init__.py
+++ qqbot/model/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from .audio import AudioControl, STATUS
-from .channel import ChannelType, ChannelSubType, PatchChannelRequest
+from .channel import Channel, ChannelType, ChannelSubType, PatchChannelRequest
 from .channel import CreateChannelRequest
 from .channel_permissions import UpdatePermission
-from .guild_member import QueryParams
+from .guild import Guild
+from .guild_member import GuildMember, QueryParams
 from .guild_role import RoleUpdateInfo
 from .message import CreateDirectMessageRequest, MessageSendRequest, Message
 from .mute import MuteOption
