6b158a2db40fe3333fe799ebccee2b20503e83da
veehou <veehou@tencent.com>
2022-02-21 19:33:55 +0800

    fix: 增加embed的demo和字段修复

---
+++ examples/demo_at_reply_embed.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import asyncio
+import os.path
+
+import qqbot
+from qqbot.core.util.yaml_util import YamlUtil
+from qqbot.model.message import MessageEmbedField, MessageEmbed
+
+test_config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yaml"))
+
+
+async def _message_handler(event, message: qqbot.Message):
+    """
+    定义事件回调的处理
+
+    :param event: 事件类型
+    :param message: 事件对象（如监听消息是Message对象）
+    """
+    await handle_send_embed(1, message.channel_id, message.id)
+
+
+async def handle_send_embed(time, channel_id, msg_id):
+    msg_api = qqbot.AsyncMessageAPI(t_token, False)
+
+    for i in range(time):
+        await asyncio.sleep(1)
+        # 构造消息发送请求数据对象
+        embed = MessageEmbed()
+        embed.title = "embed消息"
+        embed.prompt = "消息透传显示"
+
+        embed_fields = []
+        field = MessageEmbedField()
+        field.name = "<@!1234>hello world"
+        field.value = "通知提醒"
+        embed_fields.append(field)
+
+        field2 = MessageEmbedField()
+        field2.name = "<@!1234>hello world"
+        field2.value = "标题"
+        embed_fields.append(field2)
+
+        embed.fields = embed_fields
+
+        send = qqbot.MessageSendRequest(embed=embed, msg_id=msg_id, content="<@!1234>hello world")
+        # 通过api发送回复消息
+        qqbot.logger.info("send text message : %s" % i)
+        await msg_api.post_message(channel_id, send)
+
+
+if __name__ == "__main__":
+    # async的异步接口的使用示例
+    t_token = qqbot.Token(test_config["token"]["appid"], test_config["token"]["token"])
+    qqbot_handler = qqbot.Handler(
+        qqbot.HandlerType.AT_MESSAGE_EVENT_HANDLER, _message_handler
+    )
+    qqbot.async_listen_events(t_token, False, qqbot_handler)
