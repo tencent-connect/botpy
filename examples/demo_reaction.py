1da61bf650f7c5c91a30870f2e388b5cc822c245
DaiJiChen <1245487099@qq.com>
2022-03-03 22:46:57 +0800

    feat: 增加表情表态的发送与撤回能力

---
+++ examples/demo_reaction.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os.path
+
+import qqbot
+from qqbot.core.util.yaml_util import YamlUtil
+from qqbot.model.emoji import EmojiType
+
+test_config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yaml"))
+
+
+async def _reaction_handler(event, message: qqbot.Message):
+    reaction_api = qqbot.AsyncReactionAPI(t_token, False)
+    qqbot.logger.info("event %s" % event + ",receive message %s" % message.content)
+    await reaction_api.put_reaction(message.channel_id, message.id, EmojiType.system, "4")
+
+
+if __name__ == "__main__":
+    # async的异步接口的使用示例
+    t_token = qqbot.Token(test_config["token"]["appid"], test_config["token"]["token"])
+    qqbot_handler = qqbot.Handler(
+        qqbot.HandlerType.AT_MESSAGE_EVENT_HANDLER, _reaction_handler
+    )
+    qqbot.async_listen_events(t_token, False, qqbot_handler)
