e4b3414077e124e2d02f8ab2559336d3b9e1f2a7
veehou <veehou@tencent.com>
2022-03-04 13:48:08 +0800

    fix: 修复异步api调用后没有close session的问题

--- examples/demo_direct_message_reply.py
+++ examples/demo_direct_message_reply.py
@@ -8,20 +8,20 @@ from qqbot.core.util.yaml_util import YamlUtil
 test_config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yaml"))
 
 
-def _direct_message_handler(event, message: qqbot.Message):
+async def _direct_message_handler(event, message: qqbot.Message):
     """
     定义事件回调的处理
 
     :param event: 事件类型
     :param message: 事件对象（如监听消息是Message对象）
     """
-    msg_api = qqbot.DmsAPI(t_token, False)
+    msg_api = qqbot.AsyncDmsAPI(t_token, False)
     # 打印返回信息
     qqbot.logger.info("event %s" % event + ",receive message %s" % message.content)
     # 构造消息发送请求数据对象
     send = qqbot.MessageSendRequest("收到你的私信消息了：%s" % message.content, message.id)
     # 通过api发送回复消息
-    msg_api.post_direct_message(message.guild_id, send)
+    await msg_api.post_direct_message(message.guild_id, send)
 
 
 if __name__ == "__main__":
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     qqbot_handler = qqbot.Handler(
         qqbot.HandlerType.DIRECT_MESSAGE_EVENT_HANDLER, _direct_message_handler
     )
-    qqbot.listen_events(t_token, False, qqbot_handler)
+    qqbot.async_listen_events(t_token, False, qqbot_handler)
