bd6a7f9eb89af2b164a5ed0e0499785bda0bb87a
JichenDai <1245487099@qq.com>
2022-03-07 21:28:58 +0800

    feat: 机器人能够接收和发送引用消息 (#34)
    * docs: 机器人能够接收和发送引用消息
    
    * docs: 机器人能够接收和发送引用消息

---
+++ examples/demo_at_reply_reference.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os.path
+
+import qqbot
+from qqbot.core.util.yaml_util import YamlUtil
+from qqbot.model.message import MessageReference
+
+test_config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yaml"))
+
+
+async def _message_handler(event, message: qqbot.Message):
+    """
+    定义事件回调的处理
+
+    :param event: 事件类型
+    :param message: 事件对象（如监听消息是Message对象）
+    """
+    msg_api = qqbot.AsyncMessageAPI(t_token, False)
+    # 打印返回信息
+    qqbot.logger.info("event %s" % event + ",receive message %s" % message.content)
+    # 构造消息发送请求数据对象
+    message_reference = MessageReference()
+    message_reference.message_id = message.id
+    send = qqbot.MessageSendRequest(
+        content="<emoji:4>这是一条引用消息",
+        msg_id=message.id,
+        message_reference=message_reference)
+    # 通过api发送回复消息
+    await msg_api.post_message(message.channel_id, send)
+
+
+if __name__ == "__main__":
+    # async的异步接口的使用示例
+    t_token = qqbot.Token(test_config["token"]["appid"], test_config["token"]["token"])
+    qqbot_handler = qqbot.Handler(
+        qqbot.HandlerType.AT_MESSAGE_EVENT_HANDLER, _message_handler
+    )
+    qqbot.async_listen_events(t_token, False, qqbot_handler)
--- qqbot/model/message.py
+++ qqbot/model/message.py
@@ -12,12 +12,16 @@ class Message:
         self.content: str = ""
         self.timestamp: str = ""
         self.edited_timestamp: str = ""
+        self.mention_everyone: str = ""
         self.author: User = User()
         self.attachments: List[MessageAttachment] = [MessageAttachment()]
         self.embeds: List[MessageEmbed] = [MessageEmbed()]
         self.mentions: List[User] = [User()]
         self.member: Member = Member()
         self.ark: MessageArk = MessageArk()
+        self.seq: int = 0
+        self.seq_in_channel = ""
+        self.message_reference = MessageReference()
         if data:
             self.__dict__ = data
 
@@ -111,6 +115,14 @@ class MessageArk:
             self.__dict__ = data
 
 
+class MessageReference:
+    def __init__(self, data=None):
+        self.message_id = ""
+        self.ignore_get_message_error = False
+        if data:
+            self.__dict__ = data
+
+
 class MessageSendRequest:
     def __init__(
         self,
@@ -119,6 +131,7 @@ class MessageSendRequest:
         embed: MessageEmbed = None,
         ark: MessageArk = None,
         image: str = "",
+        message_reference: MessageReference = None
     ):
         """
 
@@ -127,6 +140,7 @@ class MessageSendRequest:
         :param embed:embed 消息，一种特殊的 ark
         :param ark:ark 消息
         :param image:图片url地址
+        :param message_reference:引用消息
         """
 
         self.content = content
@@ -134,6 +148,7 @@ class MessageSendRequest:
         self.ark = ark
         self.image = image
         self.msg_id = msg_id
+        self.message_reference = message_reference
 
 
 class DirectMessageGuild:
