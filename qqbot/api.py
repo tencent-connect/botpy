88a9ef12810d17076975215f3a3eb486e2b5252e
jichendai <1245487099@qq.com>
2022-04-15 13:03:15 +0800

    feat: 增加用户interaction的回调事件

--- qqbot/api.py
+++ qqbot/api.py
@@ -55,6 +55,7 @@ from qqbot.model.schedule import (
     ScheduleToCreate,
     ScheduleToPatch,
 )
+from qqbot.model.interaction import InteractionData
 from qqbot.model.token import Token
 from qqbot.model.user import ReqOption
 
@@ -896,3 +897,19 @@ class PinsAPI(APIBase):
         url = get_url(APIConstant.getPinsURI, self.is_sandbox).format(channel_id=channel_id)
         response = self.http.get(url)
         return json.loads(response.content, object_hook=PinsMessage)
+
+
+class InteractionAPI(APIBase):
+    """互动回调API"""
+
+    def put_interaction(self, interaction_id: str, interaction_data: InteractionData):
+        """
+        对 interaction_id 进行互动回调数据异步回复更新
+
+        :param interaction_id: 互动事件的ID
+        :param interaction_data: 互动事件数据体
+        """
+        url = get_url(APIConstant.interactionURI, self.is_sandbox).format(interaction_id=interaction_id)
+        request_json = JsonUtil.obj2json_serialize(interaction_data)
+        response = self.http.put(url, request_json)
+        return response.status_code == HttpStatus.NO_CONTENT
