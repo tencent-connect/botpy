330840f7c9e59df25a2439b2c3574175c1e967d6
jichendai <1245487099@qq.com>
2022-04-29 19:44:54 +0800

    feat: 简化handler的代码

--- qqbot/core/network/ws_async/ws_async_handler.py
+++ qqbot/core/network/ws_async/ws_async_handler.py
@@ -21,142 +21,48 @@ async def parse_and_handle(ws_event, data):
     event_id = ws_event["id"]
     event_type = ws_event["t"]
     context = WsContext(event_type, event_id)
-    call_handler = event_handler_dict.get(event_type)
+    callback = DefaultHandler.get_handler_by_type(event_type)
 
-    if call_handler is None:
+    if callback is None:
         if DefaultHandler.plain is not None:
             plain = DefaultHandler.plain
             asyncio.ensure_future(plain(context, data))
         else:
             return
     else:
-        asyncio.ensure_future(call_handler(context, data))
+        object_type = event_object_dict.get(event_type)
+        resolved = json.loads(_parse_data(data), object_hook=object_type)
+        asyncio.ensure_future(callback(context, resolved))
 
 
-async def _handle_event_guild(context, data):
-    callback = DefaultHandler.guild
-    if callback is None:
-        return
-    guild: Guild = json.loads(_parse_data(data), object_hook=Guild)
-    await callback(context, guild)
-
-
-async def _handle_event_channel(context, data):
-    callback = DefaultHandler.channel
-    if callback is None:
-        return
-    channel: Channel = json.loads(_parse_data(data), object_hook=Channel)
-    await callback(context, channel)
-
-
-async def _handle_event_guild_member(context, data):
-    callback = DefaultHandler.guild_member
-    if callback is None:
-        return
-    guild_member: GuildMember = json.loads(_parse_data(data), object_hook=GuildMember)
-    await callback(context, guild_member)
-
-
-async def _handle_event_at_message_create(context, data):
-    callback = DefaultHandler.at_message
-    if callback is None:
-        return
-    at_message: Message = json.loads(_parse_data(data), object_hook=Message)
-    await callback(context, at_message)
-
-
-async def _handle_event_public_message_delete(context, data):
-    callback = DefaultHandler.public_message_delete
-    if callback is None:
-        return
-    message_deletion_info: DeletedMessageInfo = json.loads(_parse_data(data), object_hook=DeletedMessageInfo)
-    await callback(context, message_deletion_info)
-
-
-async def _handle_event_message_create(context, data):
-    callback = DefaultHandler.message_create
-    if callback is None:
-        return
-    msg: Message = json.loads(_parse_data(data), object_hook=Message)
-    await callback(context, msg)
-
-
-async def _handle_event_message_delete(context, data):
-    callback = DefaultHandler.message_delete
-    if callback is None:
-        return
-    message_deletion_info: DeletedMessageInfo = json.loads(_parse_data(data), object_hook=DeletedMessageInfo)
-    await callback(context, message_deletion_info)
-
-
-async def _handle_event_direct_message_create(context, data):
-    callback = DefaultHandler.direct_message_create
-    if callback is None:
-        return
-    msg: Message = json.loads(_parse_data(data), object_hook=Message)
-    await callback(context, msg)
-
-
-async def _handle_event_direct_message_delete(context, data):
-    callback = DefaultHandler.direct_message_delete
-    if callback is None:
-        return
-    message_deletion_info: DeletedMessageInfo = json.loads(_parse_data(data), object_hook=DeletedMessageInfo)
-    await callback(context, message_deletion_info)
-
-
-async def _handle_event_audio(context, data):
-    callback = DefaultHandler.audio
-    if callback is None:
-        return
-    audio_action: AudioAction = json.loads(_parse_data(data), object_hook=AudioAction)
-    await callback(context, audio_action)
-
-
-async def _handle_event_message_reaction(context, data):
-    callback = DefaultHandler.message_reaction
-    if callback is None:
-        return
-    reaction: Reaction = json.loads(_parse_data(data), object_hook=Reaction)
-    await callback(context, reaction)
-
-
-async def _handle_event_interaction_create(context, data):
-    callback = DefaultHandler.interaction_create
-    if callback is None:
-        return
-    interaction: Interaction = json.loads(_parse_data(data), object_hook=Interaction)
-    await callback(context, interaction)
-
-
-event_handler_dict = {
-    WsEvent.EventGuildCreate: _handle_event_guild,
-    WsEvent.EventGuildUpdate: _handle_event_guild,
-    WsEvent.EventGuildDelete: _handle_event_guild,
-    WsEvent.EventChannelCreate: _handle_event_channel,
-    WsEvent.EventChannelUpdate: _handle_event_channel,
-    WsEvent.EventChannelDelete: _handle_event_channel,
-    WsEvent.EventGuildMemberAdd: _handle_event_guild_member,
-    WsEvent.EventGuildMemberUpdate: _handle_event_guild_member,
-    WsEvent.EventGuildMemberRemove: _handle_event_guild_member,
-    WsEvent.EventAtMessageCreate: _handle_event_at_message_create,
-    WsEvent.EventPublicMessageDelete: _handle_event_public_message_delete,
-    WsEvent.EventMessageCreate: _handle_event_message_create,
-    WsEvent.EventMessageDelete: _handle_event_message_delete,
-    WsEvent.EventDirectMessageCreate: _handle_event_direct_message_create,
-    WsEvent.EventDirectMessageDelete: _handle_event_direct_message_delete,
-    WsEvent.EventAudioStart: _handle_event_audio,
-    WsEvent.EventAudioFinish: _handle_event_audio,
-    WsEvent.EventAudioOnMic: _handle_event_audio,
-    WsEvent.EventAudioOffMic: _handle_event_audio,
-    WsEvent.EventMessageReactionAdd: _handle_event_message_reaction,
-    WsEvent.EventMessageReactionRemove: _handle_event_message_reaction,
-    WsEvent.EventInteractionCreate: _handle_event_interaction_create,
+event_object_dict = {
+    WsEvent.EventGuildCreate: Guild,
+    WsEvent.EventGuildUpdate: Guild,
+    WsEvent.EventGuildDelete: Guild,
+    WsEvent.EventChannelCreate: Channel,
+    WsEvent.EventChannelUpdate: Channel,
+    WsEvent.EventChannelDelete: Channel,
+    WsEvent.EventGuildMemberAdd: GuildMember,
+    WsEvent.EventGuildMemberUpdate: GuildMember,
+    WsEvent.EventGuildMemberRemove: GuildMember,
+    WsEvent.EventAtMessageCreate: Message,
+    WsEvent.EventPublicMessageDelete: DeletedMessageInfo,
+    WsEvent.EventMessageCreate: Message,
+    WsEvent.EventMessageDelete: DeletedMessageInfo,
+    WsEvent.EventDirectMessageCreate: Message,
+    WsEvent.EventDirectMessageDelete: DeletedMessageInfo,
+    WsEvent.EventAudioStart: AudioAction,
+    WsEvent.EventAudioFinish: AudioAction,
+    WsEvent.EventAudioOnMic: AudioAction,
+    WsEvent.EventAudioOffMic: AudioAction,
+    WsEvent.EventMessageReactionAdd: Reaction,
+    WsEvent.EventMessageReactionRemove: Reaction,
+    WsEvent.EventInteractionCreate: Interaction,
 }
 
 
-def _parse_data(message):
-    json_obj = json.loads(message)
+def _parse_data(data_dict):
+    json_obj = json.loads(data_dict)
     if "d" in json_obj.keys():
         data = json_obj["d"]
         return json.dumps(data)
