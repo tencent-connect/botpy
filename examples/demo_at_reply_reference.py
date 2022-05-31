dda6870ff35d109f56f08af79f93adabfeee94b5
小念同学 <2660422452@qq.com>
2022-05-31 12:05:10 +0800

    feat: 增加私信demo
    * 修改版本以及依赖package
    
    * demo update
    
    * demo update
    
    Co-authored-by: 旅行家位移 <14356413+traveler-vee@users.noreply.github.com>

---
+++ examples/demo_at_reply_reference.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+import botpy
+from botpy import logging
+from botpy.types.message import Reference
+from botpy.message import Message
+from botpy.utils import YamlUtil
+
+test_config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yaml"))
+
+_log = logging.get_logger()
+
+
+class MyClient(botpy.Client):
+    async def on_ready(self):
+        _log.info(f"robot 「{self.robot.name}」 on_ready!")
+
+    async def on_at_message_create(self, message: Message):
+        # 构造消息发送请求数据对象
+        message_reference = Reference(message_id=message.message_id)
+        # 通过api发送回复消息
+        await self.api.post_message(
+            channel_id=message.channel_id,
+            content="<emoji:4>这是一条引用消息",
+            msg_id=message.message_id,
+            message_reference=message_reference
+        )
+
+
+if __name__ == "__main__":
+    # 通过预设置的类型，设置需要监听的事件通道
+    # intents = botpy.Intents.none()
+    # intents.public_guild_messages=True
+
+    # 通过kwargs，设置需要监听的事件通道
+    intents = botpy.Intents(public_guild_messages=True)
+    client = MyClient(intents=intents)
+    client.run(appid=test_config["appid"], token=test_config["token"])
