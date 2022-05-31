5a869960b65bf210217fc8b0d0ac16c35d1d40ba
robot_nian <2660422452@qq.com>
2022-05-31 12:01:07 +0800

    demo update

--- botpy/message.py
+++ botpy/message.py
@@ -3,12 +3,13 @@ from .types import gateway
 
 
 class Message:
-    __slots__ = ("_api", "content", "channel_id", "message_id", "guild_id")
+    __slots__ = ("_api", "content", "channel_id", "message_id", "guild_id", "user_id")
 
     def __init__(self, api: BotAPI, data: gateway.MessagePayload):
         self._api = api
         # TODO 创建一些实体类的数据缓存 @veehou
         self.channel_id = data["channel_id"]
+        self.user_id = data["author"]["id"]
         self.message_id = data["id"]
         self.content = data["content"]
         self.guild_id = data["guild_id"]
