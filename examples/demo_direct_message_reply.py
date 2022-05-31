6f699deab27f6deba1e84f5d609b0c7b582e2cc8
robot_nian <2660422452@qq.com>
2022-05-31 12:00:05 +0800

    demo update

---
+++ examples/demo_direct_message_reply.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+搁置
+
+"""
+import os
+
+import botpy
+from botpy import logging
+from botpy.message import Message
+from botpy.utils import YamlUtil
+
+test_config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yaml"))
+
+_log = logging.get_logger()
+
+
+class MyClient(botpy.Client):
+    async def on_ready(self):
+        _log.info(f"robot 「{self.robot.name}」 on_ready!")
+
+    async def on_direct_message_create(self, message: Message):
+        await self.api.post_dms(
+            guild_id=message.guild_id,
+            user_id=message.user_id,
+            content=f"机器人{self.robot.name}收到你的私信了: {message.content}",
+            msg_id=message.message_id
+        )
+
+
+if __name__ == "__main__":
+    # 通过预设置的类型，设置需要监听的事件通道
+    # intents = botpy.Intents.none()
+    # intents.public_guild_messages=True
+
+    # 通过kwargs，设置需要监听的事件通道
+    intents = botpy.Intents(direct_message=True)
+    client = MyClient(intents=intents)
+    client.run(appid=test_config["appid"], token=test_config["token"])
