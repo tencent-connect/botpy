96b459dc82025e31ca2adf39e9e42682cda2748c
Neutron <64739422+Huang1220@users.noreply.github.com>
2022-06-02 11:14:09 +0800

    Update gateway.py

--- botpy/types/gateway.py
+++ botpy/types/gateway.py
@@ -37,7 +37,7 @@ class MessagePayload(TypedDict):
     seq_in_channel: str
     timestamp: str
 
-class MessageAudit(TypeDict):
+class MessageAuditPayload(TypeDict):
     audit_id: str
     message_id: str
     guild_id: str
