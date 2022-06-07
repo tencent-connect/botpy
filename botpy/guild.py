35942068decc04f080891534dd770ac4de644a88
veehou <veehou@tencent.com>
2022-06-07 11:46:39 +0800

    feat: 补齐事件下发的对象类相关属性初始化

--- botpy/guild.py
+++ botpy/guild.py
@@ -3,11 +3,30 @@ from .types import guild, gateway
 
 
 class Guild:
-    __slots__ = ("_api", "_ctx", "id", "name")
-    
+    __slots__ = (
+        "_api",
+        "_ctx",
+        "id",
+        "name",
+        "icon",
+        "owner_id",
+        "is_owner",
+        "member_count",
+        "max_members",
+        "description",
+        "joined_at",
+    )
+
     def __init__(self, api: BotAPI, ctx: gateway.WsContext, data: guild.GuildPayload):
         self._api = api
         self._ctx = ctx
-        self.id = data["id"]
-        self.name = data["name"]
-        
+
+        self.id = data.get("id")
+        self.name = data.get("name")
+        self.icon = data.get("icon")
+        self.owner_id = data.get("owner_id")
+        self.is_owner = data.get("owner", False)
+        self.member_count = data.get("member_count")
+        self.max_members = data.get("max_members")
+        self.description = data.get("description")
+        self.joined_at = data.get("joined_at")
--- botpy/message.py
+++ botpy/message.py
@@ -3,19 +3,40 @@ from .types import gateway
 
 
 class Message:
-    __slots__ = ("_api", "content", "channel_id", "message_id", "guild_id", "user_id")
+    __slots__ = (
+        "_api",
+        "author",
+        "content",
+        "channel_id",
+        "message_id",
+        "guild_id",
+        "author_id",
+        "member",
+        "mentions",
+        "seq",
+        "seq_in_channel",
+        "timestamp",
+    )
 
     def __init__(self, api: BotAPI, data: gateway.MessagePayload):
-        self._api = api
         # TODO 创建一些实体类的数据缓存 @veehou
-        self.channel_id = data["channel_id"]
-        self.user_id = data["author"]["id"]
-        self.message_id = data["id"]
-        self.content = data["content"]
-        self.guild_id = data["guild_id"]
+        self._api = api
+
+        self.author = data.get("author")
+        self.author_id = self.author.get("id")
+        self.channel_id = data.get("channel_id")
+        self.message_id = data.get("id")
+        self.content = data.get("content")
+        self.guild_id = data.get("guild_id")
+        self.member = data.get("member")
+        self.mentions = data.get("mentions")
+        self.seq = data.get("seq")  # 全局消息序号
+        self.seq_in_channel = data.get("seq_in_channel")  # 子频道消息序号
+        self.timestamp = data.get("timestamp")
 
     async def reply(self, **kwargs):
         await self._api.post_message(channel_id=self.channel_id, msg_id=self.message_id, **kwargs)
+
 
 class MessageAudit:
     __slots__ = ("_api", "audit_id", "message_id", "channel_id", "guild_id")
