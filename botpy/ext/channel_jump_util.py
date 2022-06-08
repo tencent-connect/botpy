14117fe47c4f77bff1ccd78d8e67346f5ed3f03f
single-ptilopsis <93860215+single-ptilopsis@users.noreply.github.com>
2022-06-08 12:43:25 +0800

    Update channel_jump_util.py

--- botpy/ext/channel_jump_util.py
+++ botpy/ext/channel_jump_util.py
@@ -1,9 +1,10 @@
 """
 对子频道转跳进行操作(#name)
 注意:
-1、无法识别真假转跳
-2、当子频道重名时无法准确识别
-3、当提供子频道转跳字段时请弃用本模块
+1、发送格式要求严格(#name )，自动添加的空格不能删除
+2、无法识别真假转跳
+3、当子频道重名时无法准确识别
+4、当提供子频道转跳字段时请弃用本模块
 """
 
 __all__ = [
@@ -13,17 +14,17 @@ __all__ = [
 ]
 
 import re
-from typing import List, Union, Match
+from typing import List, Dict
 
 from botpy import BotAPI
 from botpy.message import Message
 
-CHANNEL_JUMP_RE = re.compile(r"#(.{1,20}?)(?= )")
+CHANNEL_JUMP_RE = re.compile(r"#(.{1,12}?)(?= )")
 
 
 def get_channel_jump(text: str = None, message: Message = None) -> List[str]:
     """
-    识别文本中的子频道转跳
+    识别文本中的子频道转跳(粗略)
     :param message: 消息对象
     :param text: 文本，为空则message.content
     :return: 子频道名称列表(不带#)
@@ -32,43 +33,39 @@ def get_channel_jump(text: str = None, message: Message = None) -> List[str]:
 
 
 async def get_channel_jump_strict(api: BotAPI, message: Message = None, text: str = None,
-                                  guild_id: str = None) -> List[str]:
+                                  guild_id: str = None) -> Dict[str, str]:
     """
-    识别文本中的子频道转跳，并过滤不存在的子频道
+    识别文本中的子频道转跳(准确)
     :param api: BotAPI
     :param message: 消息对象
     :param text: 文本，为空则message.content
     :param guild_id: 频道id，为空则message.guild_id
-    :return: 子频道名称列表(不带#)
+    :return: {子频道名称(不带#):子频道id} （去重）
     """
-    channels = [channel["name"] for channel in await api.get_channels(guild_id or message.guild_id)]
-    return [jump for jump in CHANNEL_JUMP_RE.findall(message.content if text is None else text) if jump in channels]
+    channels = await api.get_channels(guild_id or message.guild_id)
+    text = message.content if text is None else text
+    jumps = {}
+
+    for channel in channels:
+        if "#%s " % channel["name"] in text:
+            jumps[channel["name"]] = channel["id"]
+
+    return jumps
 
 
-async def escape_channel_jump(api: BotAPI, message: Message = None, text: str = None, guild_id: str = None,
-                              strict: bool = False) -> Union[str, bool]:
+async def escape_channel_jump(api: BotAPI, message: Message = None, text: str = None, guild_id: str = None) -> str:
     """
     转义子频道转跳 (#name -> <#id>)
     :param api: BotAPI
     :param message: 消息对象
     :param text: 文本，为空则message.content
     :param guild_id: 频道id，为空则message.guild_id
-    :param strict: 是否严格模式(无不存在的子频道 返回false)
     :return: 转义后的文本
     """
     channels = await api.get_channels(guild_id or message.guild_id)
+    text = message.content if text is None else text
 
-    def _escape(match: Match):
-        for channel in channels:
-            if channel["name"] == match.group(1):
-                return "<#%s>" % channel["id"]
-        else:
-            if strict:
-                raise ValueError
-            else:
-                return match.group(0)
+    for channel in channels:
+        text = text.replace("#%s " % channel["name"], "<#%s> " % channel["id"])
 
-    try:
-        return CHANNEL_JUMP_RE.sub(_escape, message.content if text is None else text)
-    except ValueError:
-        return False
+    return text
