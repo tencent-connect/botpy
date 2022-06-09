54838c6c02c9a58820864b542ce731daefebccfe
小念 <2660422452@qq.com>
2022-06-09 23:29:21 +0800

    feat: 优化部分代码 (#90)
    * 修改日志自定义路径的方式
    
    * interaction丢失事件参数补充
    
    * 修改日志自定义路径的方式
    
    * 增加颜色转换工具,指令装饰器
    
    * 优化指令装饰器工具和子频道id查找工具
    
    * update commit
    
    * update commit

--- botpy/ext/command_util.py
+++ botpy/ext/command_util.py
@@ -1,29 +1,28 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
 
-from botpy import logger
 from botpy.message import Message
 
 
-class Command:
+class Commands:
     """
     指令装饰器
     """
 
-    def __init__(self, command: tuple or str):
-        self.command = command
+    def __init__(self, commands: tuple or str):
+        self.commands = commands
 
     def __call__(self, func):
         @wraps(func)
         async def decorated(*args, **kwargs):
             message: Message = kwargs["message"]
-            if isinstance(self.command, tuple):
-                for command in self.command:
-                    if self.command in message.content:
+            if isinstance(self.commands, tuple):
+                for command in self.commands:
+                    if self.commands in message.content:
                         # 分割指令后面的指令参数
-                        params = message.content.split(self.command)[1].strip()
+                        params = message.content.split(self.commands)[1].strip()
                         return await func(message=message, params=params)
-            elif isinstance(self.command, str):
+            elif isinstance(self.commands, str):
                 # 分割指令后面的指令参数
                 params = message.content.split(self.command)[1].strip()
                 return await func(message=message, params=params)
@@ -31,3 +30,4 @@ class Command:
                 return False
 
         return decorated
+
