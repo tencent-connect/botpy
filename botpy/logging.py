6f53bb51cea626893d4667cc84dd63466c1056de
single-ptilopsis <93860215+single-ptilopsis@users.noreply.github.com>
2022-06-10 23:12:20 +0800

    Update logging.py

--- botpy/logging.py
+++ botpy/logging.py
@@ -17,6 +17,8 @@ LOG_COLORS_CONFIG = {
     "CRITICAL": "red",
 }
 
+DEFAULT_LOGGER_NAME = "botpy"
+
 DEFAULT_PRINT_FORMAT = "\033[1;33m[%(levelname)s]\t(%(filename)s:%(lineno)s)%(funcName)s\t\033[0m%(message)s"
 DEFAULT_FILE_FORMAT = "%(asctime)s\t[%(levelname)s]\t(%(filename)s:%(lineno)s)%(funcName)s\t%(message)s"
 logging.basicConfig(format=DEFAULT_PRINT_FORMAT)
@@ -28,12 +30,12 @@ DEFAULT_FILE_HANDLER = {
     "format": "%(asctime)s\t[%(levelname)s]\t(%(filename)s:%(lineno)s)%(funcName)s\t%(message)s",
     # 可选 Default to DEBUG
     "level": logging.DEBUG,
-    # 可选，其中如有 %(name)s 会在实例化阶段填入相应的日志name
-    "filename": os.path.join(os.getcwd(), "%(name)s.log"),
     # 以下是Handler相关参数
     "when": "D",
     "backupCount": 7,
-    "encoding": "utf-8"
+    "encoding": "utf-8",
+    # *特殊* 对于filename参数，其中如有 %(name)s 会在实例化阶段填入相应的日志name
+    "filename": os.path.join(os.getcwd(), "%(name)s.log"),
 }
 
 # 存放已经获取的Logger
@@ -46,7 +48,7 @@ _ext_handlers: List[dict] = []
 os.system("")
 
 
-def get_handler(handler, name=""):
+def get_handler(handler, name=DEFAULT_LOGGER_NAME):
     """
     将handler字典实例化
     :param handler: handler配置
@@ -74,7 +76,7 @@ def get_logger(name=None):
     global logs
 
     if not name:
-        name = "botpy"
+        name = DEFAULT_LOGGER_NAME
     if name in logs:
         return logs[name]
 
@@ -108,7 +110,7 @@ def configure_logging(
     :param config: logging.config.dictConfig
     :param _format: logging.basicConfig(format=_format)
     :param level: 控制台输出level
-    :param bot_log: 是否启用bot日志 None/禁用拓展 False/全部禁用
+    :param bot_log: 是否启用bot日志 True/启用 None/禁用拓展 False/禁用拓展+控制台输出
     :param ext_handlers: 额外的handler，格式参考 DEFAULT_FILE_HANDLER。Default to True(使用默认handler)
     :param force: 是否在已追加handler(_ext_handlers)不为空时继续追加(避免因多次实例化Client类导致重复添加)
     """
@@ -135,16 +137,16 @@ def configure_logging(
     if _format is not None:
         logging.basicConfig(format=_format)
 
-    for name, logger in logs.items():
-        if level is not None:
+    if level is not None:
+        for name, logger in logs.items():
             logger.setLevel(level)
 
     if not bot_log:
-        logger = logging.getLogger("botpy")
+        logger = logging.getLogger(DEFAULT_LOGGER_NAME)
         if bot_log is False:
             logger.propagate = False
-        if "botpy" in logs:
-            logs.pop("botpy")
+        if DEFAULT_LOGGER_NAME in logs:
+            logs.pop(DEFAULT_LOGGER_NAME)
 
         logger.handlers = []
 
