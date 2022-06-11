9162c2452b71778a15dadbc088b76405e53fd68b
single-ptilopsis <93860215+single-ptilopsis@users.noreply.github.com>
2022-06-11 12:48:52 +0800

    fix: 优化logging模块 (#96)
    * Update README.md
    
    * Update client.py
    
    * Update logging.py
    
    * Update client.py

--- botpy/client.py
+++ botpy/client.py
@@ -47,7 +47,7 @@ class Client:
           log_config: 日志配置，可以为dict或.json/.yaml文件路径，会从文件中读取(logging.config.dictConfig)。Default to None（不做更改）
           log_format: 控制台输出格式(logging.basicConfig(format=))。Default to None（不做更改）
           log_level: 控制台输出level。Default to None(不做更改),
-          bot_log: bot_log: 是否启用bot日志 None/禁用拓展 False/全部禁用。Default to True
+          bot_log: bot_log: bot_log: 是否启用bot日志 True/启用 None/禁用拓展 False/禁用拓展+控制台输出
           ext_handlers: ext_handlers: 额外的handler，格式参考 logging.DEFAULT_FILE_HANDLER。Default to True(使用默认追加handler)
         """
         self.intents: int = intents.value
