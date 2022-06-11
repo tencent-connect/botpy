d5286f74196ca23d05c0e75037dd2b1615938e58
小念同学 <2660422452@qq.com>
2022-06-11 11:01:45 +0800

    feat: 为IDE增加Intents事件订阅参数列表

--- botpy/flags.py
+++ botpy/flags.py
@@ -103,6 +103,18 @@ class Flag:
 
 @fill_with_flags()
 class Intents(BaseFlags):
+    """
+    public_guild_messages	公域消息事件
+    guild_messages	消息事件 (仅 私域 机器人能够设置此 intents)
+    direct_message	私信事件
+    guild_message_reactions	消息相关互动事件
+    guilds	频道事件
+    guild_members	频道成员事件
+    interaction	互动事件
+    message_audit	消息审核事件
+    forums	论坛事件 (仅 私域 机器人能够设置此 intents)
+    audio_action	音频事件
+    """
     __slots__ = ()
 
     def __init__(self, **kwargs: bool) -> None:
