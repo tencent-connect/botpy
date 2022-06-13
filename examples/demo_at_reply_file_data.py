988beae90d7c35385fbd1e4f436bbbcb3dd0c189
veehou <veehou@tencent.com>
2022-06-13 10:20:58 +0800

    fix: 新增examples的依赖资源路径

--- examples/demo_at_reply_file_data.py
+++ examples/demo_at_reply_file_data.py
@@ -21,14 +21,14 @@ class MyClient(botpy.Client):
 
     async def on_at_message_create(self, message: Message):
         # 方法1（阅读档案后传入bytes类型图片数据）：
-        with open("test.png", "rb") as img:
+        with open("resource/test.png", "rb") as img:
             img_bytes = img.read()
         await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}", file_image=img_bytes)
         # 方法2（打开档案后直接传入档案）：
-        with open("test.png", "rb") as img:
+        with open("resource/test.png", "rb") as img:
             await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}", file_image=img)
         # 方法3（直接传入图片路径）：
-        await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}", file_image="test.png")
+        await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}", file_image="resource/test.png")
 
 
 if __name__ == "__main__":
