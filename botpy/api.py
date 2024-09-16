a5ed21ec192e470ba3ecbe905dce016e547fe9f7
RexQian <shuipoqian@gmail.com>
2024-09-16 15:13:38 +0800

    feat: 支持直接发送本地base64文件

--- botpy/api.py
+++ botpy/api.py
@@ -1475,6 +1475,7 @@ class BotAPI:
         file_type: int,
         url: str,
         srv_send_msg: bool = False,
+        file_data: str = None,
     ) -> message.Media:
         """
         上传/发送群聊图片
@@ -1484,6 +1485,7 @@ class BotAPI:
           file_type (int): 媒体类型：1 图片png/jpg，2 视频mp4，3 语音silk，4 文件（暂不开放）
           url (str): 需要发送媒体资源的url
           srv_send_msg (bool): 设置 true 会直接发送消息到目标端，且会占用主动消息频次
+          file_data (str): 要发送的本地图像的Base64数据
         """
         payload = locals()
         payload.pop("self", None)
